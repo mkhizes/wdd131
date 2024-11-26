fec60c1a13d17f7ab6939563ccb8a657f0ab62c6
mkhizes <lubanzi01trading@gmail.com>
2024-11-26 13:10:34 +0200

    ammended javascript file

--- week04/scripts/filtered-temples.js
+++ week04/scripts/filtered-temples.js
@@ -159,4 +159,15 @@ new PerformanceObserver((entryList) => {
   }
 }).observe({type: 'largest-contentful-paint', buffered: true});
 
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Hamburger Menu
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+    navList.style.display = navList.style.display === "block" ? "none" : "block";
+    hamburger.textContent = navList.style.display === "block" ? "X" : "☰";
+});
    
